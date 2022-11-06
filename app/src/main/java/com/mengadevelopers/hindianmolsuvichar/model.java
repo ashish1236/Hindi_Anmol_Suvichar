be6ba8b262fbd4502623da4bfe4b73c75fb505e0
Ashish Gangwar <Ashish790673@gmail.com>
2022-11-06 17:36:07 +0930

    Initial commit

---
+++ app/src/main/java/com/mengadevelopers/hindianmolsuvichar/model.java
@@ -0,0 +1,16 @@
+package com.mengadevelopers.hindianmolsuvichar;
+
+public class model {
+private String txt;
+    public model() {
+    }
+
+
+    public String getTxt() {
+        return txt;
+    }
+
+    public void setTxt(String txt) {
+        this.txt = txt;
+    }
+}
